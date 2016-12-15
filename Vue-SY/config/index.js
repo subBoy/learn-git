86c760349bc4c3e898a27024b0e336795bb88a3b
subBoy <15256025969@163.com>
2016-12-15 11:25:59 +0800

    初始化项目结构

--- Vue-SY/config/index.js
+++ Vue-SY/config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 9898,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
